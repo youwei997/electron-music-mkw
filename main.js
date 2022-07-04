5666a352429c1bd3b57395abdd50516c0b0c92cc
youwei <1690312648@qq.com>
2022-07-05 00:55:45 +0800

    4-1 从首页样式开始

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 
 app.on("ready", () => {
-  //  new BrowserWindow() 创建窗口
   const mainWindow = new BrowserWindow({
-    width: 800, // 窗口宽度
-    height: 600, // 窗口高度
+    width: 800,
+    height: 600,
     webPreferences: {
-      //网页功能设置
-      nodeIntegration: true, // 是否集成node
+      nodeIntegration: true,
     },
   });
-  mainWindow.loadFile("index.html"); // 加载文件
-
-  // const secondWindow = new BrowserWindow({
-  //   width: 400,
-  //   height: 300,
-  //   webPreferences: {
-  //     nodeIntegration: true,
-  //   },
-  //   parent: mainWindow, // 设置父级窗口，父级窗口关闭，当前窗口也关闭
-  // });
-  // secondWindow.loadFile("second.html");
+  mainWindow.loadFile("./renderer/index/index.html");
 });
--- renderer.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-  //Dom 渲染完即可执行 此时图片视频还可能没加载完
-  alert("DOMContentLoaded");
-});
