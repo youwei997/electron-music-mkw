812014d5b65159419c7b451db07790a08b88a8f2
youwei <1690312648@qq.com>
2022-07-06 16:13:04 +0800

    4-6 使用Electron store 持久化数据

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 
+const Store = require("electron-store");
+
+const store = new Store();
+
+console.log(app.getPath("userData"));
+
+store.set("unicorn", "🦄");
+console.log(store.get("unicorn"));
+//=> '🦄'
+
+// Use dot-notation to access nested properties
+store.set("foo.bar", true);
+console.log(store.get("foo"));
+//=> {bar: true}
+
+store.delete("unicorn");
+console.log(store.get("unicorn"));
+//=> undefined
+
 class AppWindow extends BrowserWindow {
   constructor(config, fileLocation) {
     const basicConfig = {
