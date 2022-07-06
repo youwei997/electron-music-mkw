552df32522eed5222214f9e638b9b6e2bdc0f736
youwei <1690312648@qq.com>
2022-07-06 20:11:21 +0800

    5-2 获取数据 渲染主窗口列表 编码部分

--- main.js
+++ main.js
@@ -26,6 +26,10 @@ class AppWindow extends BrowserWindow {
 
 app.on("ready", () => {
   const mainWindow = new AppWindow({}, "./renderer/index/index.html");
+  // 导航完成时触发
+  mainWindow.webContents.on("did-finish-load", () => {
+    mainWindow.send("getTracks", myStore.getTracks());
+  });
   ipcMain.on("add-music-window", () => {
     const addWindow = new AppWindow(
       {
@@ -40,7 +44,7 @@ app.on("ready", () => {
   ipcMain.on("add-tracks", (event, tracks) => {
     myStore.addTracks(tracks);
     const newTracks = myStore.getTracks();
-    console.log("add-tracks", newTracks);
+    mainWindow.send("getTracks", newTracks);
   });
 
   ipcMain.on("open-music-file", (event) => {
--- renderer/index/index.js
+++ renderer/index/index.js
@@ -1,5 +1,30 @@
 const { ipcRenderer } = require("electron");
 const { $id } = require("../helper");
+const path = require("path");
 $id("add-music-button").addEventListener("click", () => {
   ipcRenderer.send("add-music-window");
+});
+
+const renderListHTML = (tracks) => {
+  const tracksList = $id("tracks-list");
+  const tracksListHTML = tracks.reduce((html, track) => {
+    return (html += `<li class="music-track row list-group-item d-flex justify-content-between align-item-center">
+      <div class="col-10">
+        <i class="bi bi-music-note-beamed mr-2"></i>
+        <b>${track.fileName}</b>
+      </div>
+      <div class="col-2">
+        <i class="bi bi-file-play mr-2"></i>
+        <i class="bi bi-trash-fill"></i>
+      </div>
+    </li>`);
+  }, "");
+  const emptyTrackHTML = "<div class=alert alert-primary>还没有添加音乐</div>";
+  tracksList.innerHTML = tracks.length
+    ? `<ul class="list-group mt-3">${tracksListHTML}</ul>`
+    : emptyTrackHTML;
+};
+
+ipcRenderer.on("getTracks", (event, tracks) => {
+  renderListHTML(tracks);
 });
