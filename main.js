c6441b411791f35a4975c46313f032ed68da3ae3
youwei <1690312648@qq.com>
2022-07-06 14:42:40 +0800

    4-3 创建窗口类

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 
-app.on("ready", () => {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      enableRemoteModule: true,
-    },
-  });
-  mainWindow.loadFile("./renderer/index/index.html");
-  ipcMain.on("add-music-window", () => {
-    const addWindow = new BrowserWindow({
-      width: 500,
-      height: 400,
+class AppWindow extends BrowserWindow {
+  constructor(config, fileLocation) {
+    const basicConfig = {
+      width: 800,
+      height: 600,
+      show: false, // 配合下面的ready-to-show事件，会在内容加载完再显示。而不是一开始显示一个空白窗口，然后再加载内容
       webPreferences: {
         nodeIntegration: true,
         contextIsolation: false,
         enableRemoteModule: true,
       },
-      parent: mainWindow,
+    };
+    const finalConfig = { ...basicConfig, ...config };
+    super(finalConfig);
+    this.loadFile(fileLocation);
+    this.once("ready-to-show", () => {
+      this.show();
     });
-    addWindow.loadFile("./renderer/add/add.html");
+  }
+}
+
+app.on("ready", () => {
+  const mainWindow = new AppWindow({}, "./renderer/index/index.html");
+  ipcMain.on("add-music-window", () => {
+    const addWindow = new AppWindow(
+      {
+        width: 500,
+        height: 400,
+        parent: mainWindow,
+      },
+      "./renderer/add/add.html"
+    );
   });
 });
